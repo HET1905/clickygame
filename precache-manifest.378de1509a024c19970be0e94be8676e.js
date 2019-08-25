889e475460b7f9e6e43d6d1f090da0464f6ca842
Hetal Patel <nphetal1905@gmail.com>
2019-08-25 14:28:45 -0400

    Updates

---
+++ precache-manifest.378de1509a024c19970be0e94be8676e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8165d30b35408c1a904f3102113b1bb4",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "f4b2bfd3f9aba542572a",
+    "url": "/clickygame/static/css/main.b01112b0.chunk.css"
+  },
+  {
+    "revision": "1428ddcbe346e7f219a3",
+    "url": "/clickygame/static/js/2.fe5b97bd.chunk.js"
+  },
+  {
+    "revision": "f4b2bfd3f9aba542572a",
+    "url": "/clickygame/static/js/main.b39236ee.chunk.js"
+  },
+  {
+    "revision": "97ab0a19df7bee5dc929",
+    "url": "/clickygame/static/js/runtime~main.473dc39e.js"
+  },
+  {
+    "revision": "eeb1e9000f67abf173c8207da85bfd32",
+    "url": "/clickygame/static/media/pokemonBackground.eeb1e900.jpg"
+  }
+]);
